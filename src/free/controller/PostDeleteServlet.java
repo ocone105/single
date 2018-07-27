94e5a76862b809c605a9b1bad817fa79034ca7e3
kitri <kitri@DESKTOP-F6L3OQC>
2018-07-27 09:21:50 +0900

    180727 서블릿 생성 중

--- src/free/controller/PostDeleteServlet.java
+++ src/free/controller/PostDeleteServlet.java
@@ -1,39 +1,41 @@
 package free.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.eclipse.jdt.internal.compiler.parser.ParserBasicInformation;
-
-import free.dto.FreeDTO;
 import free.service.FreeService;
 import free.service.FreeServiceImpl;
-
-@WebServlet(name = "fr/read", urlPatterns = { "/fr/read.do" })
-public class PostReadServlet extends HttpServlet {
+@WebServlet(name = "fr/delete", urlPatterns = { "/fr/delete.do" })
+public class PostDeleteServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
-		System.out.println("게시글 read 서블릿 요청 성공");
+		System.out.println("서블릿 요청 성공");
 		req.setCharacterEncoding("euc-kr");
+		res.setContentType("text/html;charset=euc-kr");
+		PrintWriter out = res.getWriter();
+		System.out.println("서블릿요청성공");
 
-		// 1. 클라이언트의 요청정보 추출
+		// 1. 요청정보 추출
 		int no = Integer.parseInt(req.getParameter("no"));
 
 		// 2. 비지니스 메소드 호출
 		FreeService service = new FreeServiceImpl();
-		FreeDTO post = service.read(no);
+		int result = service.delete(no);
 
-		// 3. 데이터공유
-		req.setAttribute("post", post);
+		// 3. 응답화면생성
+		String msg = "";
+		if (result < 0) {
+			msg = "삭제실패";
+		} else {
+			msg = result + "개 행 삭제 성공!!";
+		}
 
-		// 4. 요청재지정
-		RequestDispatcher rd = req.getRequestDispatcher("/single/pages/free/freeview_view.jsp");
-		rd.forward(req, res);
+		// 요청재지정
+		res.sendRedirect("/single/fr/list.do");
 	}
 }
---
+++ src/free/controller/PostUpdateServlet.java
@@ -0,0 +1,43 @@
+package free.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import free.dto.FreeDTO;
+import free.service.FreeService;
+import free.service.FreeServiceImpl;
+
+@WebServlet(name = "fr/update", urlPatterns = { "/fr/update.do" })
+public class PostUpdateServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("게시글 수정 서블릿 요청 성공");
+		req.setCharacterEncoding("euc-kr");
+
+		// 1. 클라이언트의 요청정보 추출
+		String title = req.getParameter("title");
+		String txt = req.getParameter("txt");
+		String ctg = req.getParameter("ctg");
+
+		// 2. 비지니스 메소드 호출
+		FreeDTO post = new FreeDTO(title, txt, ctg);
+		System.out.println(post);
+
+		FreeService service = new FreeServiceImpl();
+		int result = service.update(post);
+
+		// 4. 요청재지정 - forward
+		String view = "";
+		if (result >= 1) {
+			view = "/single/fr/list.do";
+		} else {
+			// view = "/serverweb/emp/read.do?id=" + id + "&action=UPDATE";
+		}
+		res.sendRedirect(view);
+
+	}
+}
